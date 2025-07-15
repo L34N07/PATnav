ecc5f648d890e30e96981e880510ed80f15f424d
L34N07 <142454912+L34N07@users.noreply.github.com>
2025-07-15 19:00:12 -0300

    Simplify dependencies and update UI (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div style={{ padding: 20 }}>
-      <h1>Hello world from TSX inside Electron 🎉</h1>
+      <h1>PATnav UI</h1>
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   )
 }
