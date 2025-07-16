740df45dc7c632bbe1370fc90ef77a5030915746
L34N07 <142454912+L34N07@users.noreply.github.com>
2025-07-15 21:04:03 -0300

    Improve UI and behavior

--- main.js
+++ main.js
@@ -25,5 +25,5 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow)
-app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit() })
+app.on('window-all-closed', () => { app.quit() })
 app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow() })
